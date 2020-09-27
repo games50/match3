df4c6417454825b6ed7d31564ab2806d9a7ef5fd
Colton Ogden <coltonoscopy@gmail.com>
2020-09-27 14:13:26 -0700

    incorrect rgb color values for game over state

--- match-3/src/states/GameOverState.lua
+++ match-3/src/states/GameOverState.lua
@@ -29,10 +29,10 @@ end
 function GameOverState:render()
     love.graphics.setFont(gFonts['large'])
 
-    love.graphics.setColor(56, 56, 56, 234)
+    love.graphics.setColor(56/255, 56/255, 56/255, 234/255)
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 64, 64, 128, 136, 4)
 
-    love.graphics.setColor(99, 155, 255, 255)
+    love.graphics.setColor(99/255, 155/255, 255/255, 255/255)
     love.graphics.printf('GAME OVER', VIRTUAL_WIDTH / 2 - 64, 64, 128, 'center')
     love.graphics.setFont(gFonts['medium'])
     love.graphics.printf('Your Score: ' .. tostring(self.score), VIRTUAL_WIDTH / 2 - 64, 140, 128, 'center')
